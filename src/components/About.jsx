e024354a2e6da7392399cf4604ff83e4f228e21f
Ziad Osama <osamaziad417@gmail.com>
2025-09-28 15:40:40 +0300

    Fix SelfImg.jpg filename case sensitivity

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ function About() {
         <p className="text-xl font-pixel w-full sm:w-[500px] px-4 leading-tight md:relative md:px-0 lg:text-2xl lg:left-[35vh] lg:top-[15vh]">
           Hello, my name is Ziad. I'm a 18 year old student from Egypt. I'm a front-end developer, still in the learning process. I'm actively learning new things and improving my skills.
         </p>
-        <img className="relative left-[140vh] bottom-[30vh] rounded-full w-[240px] h-[240px]" src="/src/assets/Selfimg.jpg" alt="Image.jpg" />
+        <img className="relative left-[140vh] bottom-[30vh] rounded-full w-[240px] h-[240px]" src="/src/assets/SelfImg.jpg" alt="Image.jpg" />
       </div>
     </div>
   )
